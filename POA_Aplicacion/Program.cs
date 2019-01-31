af8938242438c9e0742500ea8f14c58adb653209
Wilson Dario Campaña Diaz <mouse7522@hotmail.com>
2019-01-31 05:26:16 -0500

    Agregar archivos de proyecto.

---
+++ POA_Aplicacion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POA_Aplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
