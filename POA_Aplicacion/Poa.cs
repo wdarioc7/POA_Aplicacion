2c1fc2e9951bd6fe8eb13834009ce66e5465e7b3
Wilson Dario Campaña Diaz <mouse7522@hotmail.com>
2019-02-06 06:54:15 -0500

    cambios

--- POA_Aplicacion/Poa.cs
+++ POA_Aplicacion/Poa.cs
@@ -1,15 +1,28 @@
 ﻿
 using PostSharp.Aspects;
 using System;
+using System.Diagnostics;
 
 namespace PruebaPostSharp
 {
     [Serializable]
-    class AspectoQueInterceptaUnMetodo : OnMethodBoundaryAspect
+    public sealed class AspectoQueInterceptaUnMetodo : OnMethodBoundaryAspect
     {
+        private readonly string _category;
+        public AspectoQueInterceptaUnMetodo(string category)
+        {
+            _category = category;
+        }
         public override void OnEntry(MethodExecutionArgs args)
         {
             Console.WriteLine("Entrado al método: " + args.Method.Name);
+            Trace.WriteLine(string.Format("Entering:{0}.{1}",args.Method.DeclaringType.Name,args.Method.Name),_category);
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Entrado al método: " + args.Method.Name);
+            Trace.WriteLine(string.Format("Entering:{0}.{1}", args.Method.DeclaringType.Name, args.Method.Name), _category);
         }
     }
 }
--- POA_Aplicacion/Program.cs
+++ POA_Aplicacion/Program.cs
@@ -3,15 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using PostSharp.Aspects;
+using static POA_Aplicacion.Program;
 
 namespace POA_Aplicacion
 {
     class Program
     {
-        [PruebaPostSharp.AspectoQueInterceptaUnMetodo]
+       
         static void Main(string[] args)
         {
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Hola();
+            Adios();
+        }
+        [Trace("HolaCategoria")]
+        public static void Hola ()
+            {
             Console.WriteLine("Hola Mundo!");
+        }
+        [Trace("AdiosCategoria")]
+        public static void Adios()
+        {
+            Console.WriteLine("Adios Mundo!");
+        }
+        [Serializable]
+        public sealed class TraceAttribute : OnMethodBoundaryAspect
+        {
+            private readonly string _category;
+            public TraceAttribute(string category)
+            {
+                _category = category;
+            }
+            public override void OnEntry(MethodExecutionArgs args)
+            {
+                //Console.WriteLine("Entrado al método: " + args.Method.Name);
+                Trace.WriteLine(string.Format("Entering:{0}.{1}", args.Method.DeclaringType.Name, args.Method.Name), _category);
+            }
+
+            public override void OnExit(MethodExecutionArgs args)
+            {
+                //Console.WriteLine("Entrado al método: " + args.Method.Name);
+                Trace.WriteLine(string.Format("Entering:{0}.{1}", args.Method.DeclaringType.Name, args.Method.Name), _category);
+            }
         }
     }
 }
+
